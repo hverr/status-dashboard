71b1a59c231b0cc0a132540cf4a865f87087f297
Henri Verroken <henriverroken@gmail.com>
2015-08-03 13:46:13 +0200

    Remove unused angular components

--- static/app/app.js
+++ static/app/app.js
@@ -5,7 +5,6 @@ angular.module('myApp', [
   'ngRoute',
   'myApp.view1',
   'myApp.view2',
-  'myApp.version'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
