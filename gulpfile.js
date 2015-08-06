157aa545d69f98f76754760f2f66c5e1f8ead438
Henri Verroken <henriverroken@gmail.com>
2015-08-06 19:43:07 +0200

    Add gulp-recess to lint less stylesheets

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,20 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
+var recess = require('gulp-recess');
 
-gulp.task('hint', function() {
+gulp.task('lint', ['jshint', 'recess'], function() {});
+
+gulp.task('jshint', function() {
   gulp.src(["static/app/**/*.js", '!static/app/bower_components/**'])
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
+});
+
+gulp.task('recess', function() {
+  options = {
+    strictPropertyOrder: false,
+  };
+  gulp.src(["static/app/**/*.less", '!static/app/bower_components/**'])
+    .pipe(recess(options))
+    .pipe(recess.reporter());
 });
