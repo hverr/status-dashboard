70e33e5f9e40556f2bf75fbb090ddcd8ae1d075b
Henri Verroken <henriverroken@gmail.com>
2015-08-08 17:23:42 +0200

    Fix load_test fail messages

--- widgets/load_test.go
+++ widgets/load_test.go
@@ -8,10 +8,10 @@ import (
 
 func TestGatherCores(t *testing.T) {
 	_, err := GatherCores()
-	assert.Nil(t, err, "Did not expect error:", err)
+	assert.Nil(t, err, "Did not expect error: %v", err)
 }
 
 func TestGatherLoadAverage(t *testing.T) {
 	_, _, _, err := GatherLoadAverage()
-	assert.Nil(t, err, "Did not expect error:", err)
+	assert.Nil(t, err, "Did not expect error: %v", err)
 }
