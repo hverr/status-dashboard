4fa273ea66c2f3e60b368a813244160fdad88318
Henri Verroken <henriverroken@gmail.com>
2015-08-09 16:02:01 +0200

    Add WidgetController

--- server/static/app/widgets/controllers.js
+++ server/static/app/widgets/controllers.js
@@ -8,5 +8,17 @@ angular.module('dashboard').controller('GridController', [
     widgetsManager.register(new Widget('webserver', 'load', {
       sizeX: 1, sizeY: 1, row: 0, col: 0,
     }));
+
+    $scope.widgets = [
+      { sizeX: 1, sizeY: 1, row: 0, col: 0, data: "hay"}
+    ];
+  }
+]);
+
+angular.module('dashboard').controller('WidgetController', [
+  '$log',
+  '$scope',
+  function($log, $scope) {
+    $log.debug("WidgetController:", $scope.widget);
   }
 ]);
