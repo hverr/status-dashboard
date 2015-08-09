e84582198df9e2cfa8faede6171008cae799cec8
Henri Verroken <henriverroken@gmail.com>
2015-08-09 17:37:08 +0200

    Use a grid with 4 columns

--- server/static/app/widgets/controllers.js
+++ server/static/app/widgets/controllers.js
@@ -9,6 +9,10 @@ angular.module('dashboard').controller('GridController', [
       sizeX: 1, sizeY: 1, row: 0, col: 0,
     }));
 
+    $scope.gridsterOpts = {
+      columns: 4,
+    };
+
     $scope.widgets = [
       { sizeX: 1, sizeY: 1, row: 0, col: 0, data: { client : "Webserver", name : "Load", directive: "load-widget" }},
       { sizeX: 1, sizeY: 1, row: 0, col: 1, data: { client : "Webserver", name : "Uptime", directive: "uptime-widget" }},
