a40afe1d86119c05d2d5f6610ad67efd8e336bae
Henri Verroken <henriverroken@gmail.com>
2015-08-09 14:16:31 +0200

    Gulp task to trim lines

--- server/gulpfile.js
+++ server/gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var recess = require('gulp-recess');
+var trimlines = require('gulp-trimlines');
 
 gulp.task('lint', ['jshint', 'recess'], function() {});
 
@@ -11,10 +12,25 @@ gulp.task('jshint', function() {
 });
 
 gulp.task('recess', function() {
-  options = {
+  var options = {
     strictPropertyOrder: false,
   };
   gulp.src(["static/app/**/*.less", '!static/app/bower_components/**'])
     .pipe(recess(options))
     .pipe(recess.reporter());
+});
+
+gulp.task('trimlines', function() {
+  var options = {
+    leading: false,
+  };
+  var source = [
+    "static/app/**/*.less",
+    "static/app/**/*.html",
+    "static/app/**/*.js",
+    "!static/app/bower_components/**",
+  ];
+  gulp.src(source)
+    .pipe(trimlines(options))
+    .pipe(gulp.dest("static/app"));
 });
