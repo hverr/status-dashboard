c09cf502634c59e6748bdf5685e1da03e78e44dd
Henri Verroken <henriverroken@gmail.com>
2015-08-09 16:27:19 +0200

    Display client and widget name

--- server/static/app/widgets/controllers.js
+++ server/static/app/widgets/controllers.js
@@ -10,8 +10,8 @@ angular.module('dashboard').controller('GridController', [
     }));
 
     $scope.widgets = [
-      { sizeX: 1, sizeY: 1, row: 0, col: 0, data: "hay"},
-      { sizeX: 1, sizeY: 1, row: 0, col: 1, data: "joo"},
+      { sizeX: 1, sizeY: 1, row: 0, col: 0, data: { client : "Webserver", name : "Load" }},
+      { sizeX: 1, sizeY: 1, row: 0, col: 1, data: { client : "Webserver", name : "Uptime" }},
     ];
   }
 ]);
@@ -20,6 +20,7 @@ angular.module('dashboard').controller('WidgetController', [
   '$log',
   '$scope',
   function($log, $scope) {
-    $log.debug("WidgetController:", $scope.widget);
+    $log.debug("WidgetController:", $scope.widget.data);
+    $scope.widget = $scope.widget.data;
   }
 ]);
