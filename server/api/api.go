842bc540b35b7b6f4c4994b49ed995cabe782e8a
Henri Verroken <henriverroken@gmail.com>
2015-08-09 12:19:38 +0200

    Remove superfluous comments

--- server/api/api.go
+++ server/api/api.go
@@ -7,9 +7,6 @@ import "github.com/gin-gonic/gin"
 // Some end points are used by the angular app to query for widget information,
 // others are used by clients to push widget information.
 func Install(engine *gin.Engine) error {
-
-	// POST /api/clients/register -> registerClient
-	// POST /api/clients/:client/widgets/:widget/update -> updateClientWidget
 	engine.POST("/api/clients/:client/register", registerClient)
 	engine.POST("/api/clients/:client/widgets/:widget/update", updateClientWidget)
 	engine.GET("/api/clients/:client/widgets/requested", requestedClientWidgets)
