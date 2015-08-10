d7cc28e8e048bd90095db07506bf726a238fdc10
Henri Verroken <henriverroken@gmail.com>
2015-08-11 00:02:17 +0200

    Use bulk update with long polling in angular app

--- server/static/app/widgets/services.js
+++ server/static/app/widgets/services.js
@@ -1,33 +1,75 @@
 'use strict';
 
 angular.module('dashboard').factory('widgetsManager', [
-  '$interval',
+  '$timeout',
   'api',
   '$log',
-  function($interval, api, $log) {
-    var widgets = [];
+  function($timeout, api, $log) {
+    var clients = {};
+
+    function registeredWidgets() {
+      var widgets = [];
+      for(var clientIdentifier in clients) {
+        var client = clients[clientIdentifier];
+        for(var widgetType in client) {
+          widgets.push(client[widgetType]);
+        }
+      }
+
+      return widgets;
+    }
 
     return {
       register : function(widget) {
-        $log.info('Registering widget', widget.type, 'for', widget.client);
-        widgets.push(widget);
+        $log.info('Registering', widget);
+        if(clients[widget.client] === undefined) {
+          clients[widget.client] = {};
+        }
+        clients[widget.client][widget.identifier] = widget;
       },
 
-      registeredWidgets : function() {
-        return widgets;
-      },
+      registeredWidgets : registeredWidgets,
 
       start : function() {
-        var f = function() {
-          widgets.forEach(function(w) {
-            api.widget(w).then(function(result) {
-              w.update(result);
-            });
+        var update = function(force) {
+          var request = {};
+
+          for(var clientIdentifier in clients) {
+            request[clientIdentifier] = [];
+            for(var widgetType in clients[clientIdentifier]) {
+              request[clientIdentifier].push(widgetType);
+            }
+          }
+
+          api.bulk(request, force).then(function(result) {
+            if(!result) {
+              // A timeout occurred.
+              $timeout(update, 100);
+              return;
+            }
+
+            for(var clientIdentifier in result) {
+              if(!clients[clientIdentifier]) {
+                continue;
+              }
+
+              var client = clients[clientIdentifier];
+              for(var widgetType in result[clientIdentifier]) {
+                if(!client[widgetType]) {
+                  continue;
+                }
+
+                client[widgetType].update(result[clientIdentifier][widgetType]);
+              }
+            }
+
+            $timeout(update, 100);
+          }, function() {
+            $timeout(update, 10*1000);
           });
         };
 
-        f();
-        $interval(f, 5*1000);
+        update(true);
       },
     };
   }
@@ -52,11 +94,39 @@ angular.module('dashboard').factory('api', [
         $http
           .get(r)
           .then(function(result) {
-            $log.info('Updated', w.identifier, 'for', w.client, ':', result);
             deferred.resolve(result.data);
 
           }, function(reason) {
             $log.error('error: api.widget:', reason);
+            deferred.reject(reason);
+          });
+
+        return deferred.promise;
+      },
+
+      bulk: function(widgets, force) {
+        var deferred = $q.defer();
+
+        var r = this.resource('/all_widgets');
+        if(force) {
+          r += '?force=true';
+        }
+
+        $http
+          .post(r, widgets)
+          .then(function(result) {
+            $log.debug('Updated widgets:', result);
+            if(result.status === 202) {
+              // Long poll timout
+              deferred.resolve({});
+
+            } else {
+              deferred.resolve(result.data);
+            }
+
+          }, function(reason) {
+            $log.error('error: api.bulk:', reason);
+            deferred.reject(reason);
           });
 
         return deferred.promise;
