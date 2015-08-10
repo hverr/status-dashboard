380253366095c23de16a1f09948557ba8bf08577
Henri Verroken <henriverroken@gmail.com>
2015-08-10 21:56:22 +0200

    Make uptime widget more accurate in server side

--- widgets/uptime.go
+++ widgets/uptime.go
@@ -2,18 +2,21 @@ package widgets
 
 import (
 	"bufio"
+	"encoding/json"
 	"errors"
 	"os"
 	"strconv"
+	"time"
 )
 
 const UptimeWidgetType = "uptime"
 
 type UptimeWidget struct {
-	Days    int `json:"days"`
-	Hours   int `json:"hours"`
-	Minutes int `json:"minutes"`
-	Seconds int `json:"seconds"`
+	LastUpdated time.Time
+	Days        int
+	Hours       int
+	Minutes     int
+	Seconds     int
 }
 
 func (widget *UptimeWidget) Name() string {
@@ -26,6 +29,56 @@ func (widget *UptimeWidget) Type() string {
 
 func (widget *UptimeWidget) HasData() bool {
 	return true
+}
+
+func (widget *UptimeWidget) UnmarshalJSON(data []byte) error {
+	var helper struct {
+		Days    int `json:"days"`
+		Hours   int `json:"hours"`
+		Minutes int `json:"minutes"`
+		Seconds int `json:"seconds"`
+	}
+
+	if err := json.Unmarshal(data, &helper); err != nil {
+		return err
+	}
+
+	widget.Days = helper.Days
+	widget.Hours = helper.Hours
+	widget.Minutes = helper.Minutes
+	widget.Seconds = helper.Seconds
+	widget.LastUpdated = time.Now()
+
+	return nil
+}
+
+func (widget *UptimeWidget) MarshalJSON() ([]byte, error) {
+	var helper struct {
+		Days    int `json:"days"`
+		Hours   int `json:"hours"`
+		Minutes int `json:"minutes"`
+		Seconds int `json:"seconds"`
+	}
+
+	total := CombineToSeconds(
+		widget.Days,
+		widget.Hours,
+		widget.Minutes,
+		widget.Seconds,
+	)
+
+	if !widget.LastUpdated.IsZero() {
+		total += int(time.Since(widget.LastUpdated).Seconds())
+	}
+
+	d, h, m, s := SplitSeconds(total)
+
+	helper.Days = d
+	helper.Hours = h
+	helper.Minutes = m
+	helper.Seconds = s
+
+	return json.Marshal(helper)
 }
 
 func (widget *UptimeWidget) Update() error {
@@ -62,6 +115,14 @@ func GatherUptime() (days, hours, minutes, seconds int, err error) {
 		return
 	}
 
+	days, hours, minutes, seconds = SplitSeconds(seconds)
+
+	return
+}
+
+func SplitSeconds(in int) (days, hours, minutes, seconds int) {
+	seconds = in
+
 	days = seconds / (60 * 60 * 24)
 	seconds -= days * (60 * 60 * 24)
 
@@ -72,4 +133,12 @@ func GatherUptime() (days, hours, minutes, seconds int, err error) {
 	seconds -= minutes * 60
 
 	return
+}
+
+func CombineToSeconds(days, hours, minutes, seconds int) int {
+	hours += (days * 24)
+	minutes += (hours * 60)
+	seconds += (minutes * 60)
+
+	return seconds
 }
