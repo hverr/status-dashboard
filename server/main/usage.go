93a153698d5a9c3c5cef8b4d1e4ffd736457dd82
Henri Verroken <henriverroken@gmail.com>
2015-09-03 23:58:39 +0200

    Add map of login:password pairs to configuration

--- server/main/usage.go
+++ server/main/usage.go
@@ -19,9 +19,13 @@ CONFIGURATION FILE:
 				"secret" : "supersecretkey"
 			},
 			"mysql" : {}
+		},
+		"users" : {
+		  "username" : "password"
 		}
 	}
 
 	**updateInterval**: Minimum time between update requests. (optional)
 	**clients**: List of allowed clients and their configuration.
+	**users**: Dictionary of login:password pairs for users that are allowed to access the dashboard. (optional)
 `
