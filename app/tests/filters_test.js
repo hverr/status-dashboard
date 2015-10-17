a70dda248b8e6db9b8ba875c25714c989b819738
Henri Verroken <henriverroken@gmail.com>
2015-10-17 10:29:45 +0200

    Filter to make human readable byte size and bandwidth

--- app/tests/filters_test.js
+++ app/tests/filters_test.js
@@ -26,4 +26,67 @@ describe('general filter', function() {
       expect(pluralize(0, "dog")).to.equal("dogs");
     });
   });
+
+  describe('byteSize', function() {
+    var byteSize;
+
+    beforeEach(inject(function($filter) {
+      byteSize = $filter('byteSize');
+    }));
+
+    it('should use a precision of at least 1', function() {
+      expect(byteSize(5)).to.equal('5.0 B');
+      expect(byteSize(12, 0)).to.equal('12.0 B');
+      expect(byteSize("12.3", 0)).to.equal('12.3 B');
+    });
+
+    it('should handle invalid bytes', function() {
+      expect(byteSize('not a number')).to.equal('0.0 B');
+      expect(byteSize(0)).to.equal('0.0 B');
+    });
+
+    it('should handle B range', function() {
+      expect(byteSize(0)).to.equal('0.0 B');
+      expect(byteSize(50)).to.equal('50.0 B');
+      expect(byteSize(1023)).to.equal('1023.0 B');
+    });
+
+    it('should handle kB range', function() {
+      expect(byteSize(1024)).to.equal('1.0 kB');
+      expect(byteSize(2048)).to.equal('2.0 kB');
+    });
+
+    it('should handle MB range', function() {
+      expect(byteSize(1*1024*1024)).to.equal('1.0 MB');
+      expect(byteSize(2*1024*1024)).to.equal('2.0 MB');
+    });
+
+    it('should handle GB range', function() {
+      expect(byteSize(1*1024*1024*1024)).to.equal('1.0 GB');
+      expect(byteSize(2*1024*1024*1024)).to.equal('2.0 GB');
+    });
+
+    it('should handle PB range', function() {
+      expect(byteSize(1*1024*1024*1024*1024)).to.equal('1.0 PB');
+      expect(byteSize(2*1024*1024*1024*1024)).to.equal('2.0 PB');
+    });
+
+    it('should handle >PB range', function() {
+      expect(byteSize(2*1024*1024*1024*1024*1024)).to.equal('2048.0 PB');
+    });
+  });
+
+  describe('bandwidth', function() {
+    var bandwidth;
+
+    beforeEach(inject(function($filter) {
+      bandwidth = $filter('bandwidth');
+    }));
+
+    it('should work correctly', function() {
+      expect(bandwidth(2048, 1.0, 2)).to.equal('2.00 kB/s');
+      expect(bandwidth(2048, 2.0)).to.equal('1.0 kB/s');
+      expect(bandwidth(2*1024*1024)).to.equal('2.0 MB/s');
+    });
+  });
 });
--- app/widgets/filters.js
+++ app/widgets/filters.js
@@ -13,3 +13,47 @@ angular.module('dashboard').filter('widgetName', [
     };
   }
 ]);
+
+angular.module('dashboard').filter('byteSize', [
+  function() {
+    return function(bytes, precision) {
+      if(!precision) {
+        precision = 1;
+      }
+
+      bytes = parseFloat(bytes);
+      if(!bytes) {
+        bytes = 0;
+      }
+
+      var i, suffixes = ['B', 'kB', 'MB', 'GB', 'PB'];
+      for(i = 0; i < suffixes.length; i++) {
+        if(bytes < 1024) {
+          break;
+        }
+        bytes /= 1024;
+      }
+
+      if(i === suffixes.length) {
+        bytes *= 1024;
+        i -= 1;
+      }
+
+      return bytes.toFixed(precision) + ' ' + suffixes[i];
+    };
+  }
+]);
+
+angular.module('dashboard').filter('bandwidth', [
+  'byteSizeFilter',
+  function(byteSizeFilter) {
+    return function(bytes, time, precision) {
+      if(!time) {
+        time = 1.0;
+      }
+
+      var bytesPerSecond = bytes / time;
+      return byteSizeFilter(bytesPerSecond, precision) + '/s';
+    };
+  }
+]);
