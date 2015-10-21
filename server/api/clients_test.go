f69bf195b4d7bc12b44b00bacaa4fc93e755690a
Henri Verroken <henriverroken@gmail.com>
2015-10-21 11:33:37 +0200

    Test bulk update API

--- server/api/clients_test.go
+++ server/api/clients_test.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"net/http"
@@ -10,6 +11,7 @@ import (
 
 	"github.com/hverr/status-dashboard/server"
 	"github.com/hverr/status-dashboard/server/scheduler"
+	"github.com/hverr/status-dashboard/widgets"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -154,6 +156,118 @@ func TestRequestedClientWidgets(t *testing.T) {
 		reqUrl := "/api/clients/unkown-client/requested_widgets"
 		resp := testApi.call("GET", reqUrl, "", nil)
 		assert.Equal(t, http.StatusNotFound, resp.Code)
+	}
+}
+
+func TestBulkUpdateClient(t *testing.T) {
+	// Setup
+	testApi := validApi()
+	reqUrl := fmt.Sprintf("/api/clients/%s/bulk_update", TestClientIdentifier)
+	p := []map[string]interface{}{
+		{ // normal widget
+			"type":       "load",
+			"identifier": "load",
+			"widget": map[string]interface{}{
+				"one":     "0.0",
+				"five":    "0.01",
+				"fifteen": "0.05",
+				"cores":   1,
+			},
+		},
+		{ // normal widget
+			"type":       "network",
+			"identifier": "network_eth0",
+			"widget": map[string]interface{}{
+				"interface":   "eth0",
+				"interval":    float64(1.5),
+				"received":    int(10),
+				"transmitted": int(90100200),
+			},
+		},
+		{ // missing widget
+			"type":       "",
+			"identifier": "connections",
+			"widget":     nil,
+		},
+	}
+
+	// Register client
+	testApi.Scheduler.RegisterClient(TestClientIdentifier)
+
+	r := &server.ClientRegistration{
+		Name:             TestClientName,
+		Identifier:       TestClientIdentifier,
+		AvailableWidgets: nil,
+	}
+	err := testApi.Server.RegisterClient(r)
+	require.Nil(t, err, "Unexpected error: %v", err)
+
+	// Set widget that is missing
+	client, _ := testApi.Server.GetClient(TestClientIdentifier)
+	require.NotNil(t, client)
+	client.SetWidget(&widgets.ConnectionsWidget{10, 20})
+
+	// Test all valid
+	{
+		resp := testApi.callJSON("POST", reqUrl, TestClientSecret, p)
+		assert.Equal(t, http.StatusOK, resp.Code)
+
+		assert.NotNil(t, client.GetWidget("network_eth0"))
+		assert.NotNil(t, client.GetWidget("load"))
+		assert.Nil(t, client.GetWidget("connections"))
+	}
+
+	// Test unknown widget type
+	{
+		p := []map[string]interface{}{
+			{
+				"type":       "unknown-type",
+				"identifier": "identifier",
+				"widget":     map[string]string{"key": "value"},
+			},
+		}
+
+		resp := testApi.callJSON("POST", reqUrl, TestClientSecret, p)
+		assert.Equal(t, http.StatusNotFound, resp.Code)
+	}
+
+	// Test bad widget data json
+	{
+		p := []map[string]interface{}{
+			{
+				"type":       "load",
+				"identifier": "load",
+				"widget": map[string]interface{}{
+					"one":     "0.0",
+					"five":    "0.01",
+					"fifteen": "0.05",
+					"cores":   "not-an-integer-thus-invalid",
+				},
+			},
+		}
+
+		resp := testApi.callJSON("POST", reqUrl, TestClientSecret, p)
+		assert.Equal(t, http.StatusBadRequest, resp.Code)
+	}
+
+	// Test invalid client secret
+	{
+		resp := testApi.callJSON("POST", reqUrl, "invalid secret", p)
+		assert.Equal(t, http.StatusUnauthorized, resp.Code)
+	}
+
+	// Test invalid client
+	{
+		reqUrl := "/api/clients/unknown-client/bulk_update"
+		resp := testApi.callJSON("POST", reqUrl, "", p)
+		assert.Equal(t, http.StatusNotFound, resp.Code)
+	}
+
+	// Test invalid json
+	{
+		body := bytes.NewBufferString("invalid json body")
+		resp := testApi.call("POST", reqUrl, TestClientSecret, body)
+		assert.Equal(t, http.StatusBadRequest, resp.Code)
 	}
 }
 
