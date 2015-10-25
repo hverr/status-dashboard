5f6eef881ac997f97f868ddee25e5294d2c6d385
Henri Verroken <henriverroken@gmail.com>
2015-10-25 12:35:35 +0100

    Use [+]/[-]/[*] prefix for update messages

--- updates/updates.go
+++ updates/updates.go
@@ -21,28 +21,28 @@ const (
 func CheckForUpdates(baseName string) {
 	u := appUpdater(baseName, nil)
 
-	fmt.Println("Checking for updates...")
+	fmt.Println("[+] Checking for updates...")
 	latest, err := u.Check()
 	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error:", err)
-		fmt.Println("Failed to check for updates.")
+		fmt.Println("[-] Error:", err)
+		fmt.Println("[-] Failed to check for updates.")
 		os.Exit(1)
 	}
 
 	if latest == nil {
-		fmt.Println("This version is up to date.")
+		fmt.Println("[*] This version is up to date.")
 		os.Exit(0)
 	}
 
-	fmt.Printf("A new version is availeble: %v (%v):\n", latest.Name(), latest.Identifier())
+	fmt.Printf("[*] A new version is available: %v (%v):\n", latest.Name(), latest.Identifier())
 	fmt.Println(latest.Information())
 	fmt.Println("")
 
 	if version.HasVersionInformation() {
-		fmt.Print("Current version: ")
+		fmt.Print("[*] Current version: ")
 		version.PrintVersionInformation(os.Stdout)
 	} else {
-		fmt.Println("No version is available for the current binary.")
+		fmt.Println("[*] No version is available for the current binary.")
 	}
 }
 
@@ -50,47 +50,47 @@ func UpdateApp(baseName string) {
 	path, err := osext.Executable()
 	if err != nil || path == "" {
 		if err != nil {
-			fmt.Fprintln(os.Stderr, "Erorr:", err)
+			fmt.Println("[-] Error:", err)
 		}
-		fmt.Println("Failed to get the executable location.")
+		fmt.Println("[-] Failed to get the executable location.")
 		os.Exit(1)
 	}
 	f := updater.NewDelayedFile(path)
 	u := appUpdater(baseName, f)
 
-	fmt.Println("Checking for updates...")
+	fmt.Println("[+] Checking for updates...")
 	latest, err := u.Check()
 	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error:", err)
-		fmt.Println("Failed to check for updates.")
+		fmt.Println("[-] Error:", err)
+		fmt.Println("[-] Failed to check for updates.")
 		os.Exit(1)
 	}
 
 	if latest == nil {
-		fmt.Println("This version is up to date.")
+		fmt.Println("[*] This version is up to date.")
 		os.Exit(0)
 	}
 
-	fmt.Printf("Downloading %v (%v):\n", latest.Name(), latest.Identifier())
+	fmt.Printf("[+] Downloading %v (%v):\n", latest.Name(), latest.Identifier())
 	fmt.Println(latest.Information())
 	fmt.Println("")
 
 	err = u.UpdateTo(latest)
 	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error:", err)
-		fmt.Println("An error ocurred while updating.")
+		fmt.Println("[-] Error:", err)
+		fmt.Println("[-] An error ocurred while updating.")
 		os.Exit(1)
 	}
 
-	fmt.Println("Replacing", path)
+	fmt.Println("[+] Replacing", path)
 	err = f.Close()
 	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error:", err)
-		fmt.Println("An error ocurred while updating.")
+		fmt.Println("[-] Error:", err)
+		fmt.Println("[-] An error ocurred while updating.")
 		os.Exit(1)
 	}
 
-	fmt.Println("Successfully updated the binary.")
+	fmt.Println("[+] Successfully updated the binary.")
 }
 
 func appUpdater(baseName string, f updater.AbortWriter) updater.Updater {
